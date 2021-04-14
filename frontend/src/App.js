675cbd5f85bb5cd691e11d09e79fd85f378476bf
Fagner Lopes <fagnernlopes@gmail.com>
2021-04-13 22:12:57 -0300

    Upload initial files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+export default function App() {
+  
+
+  return (
+    <div>
+      Teste
+    </div>
+  );
+}
---
+++ frontend/src/api/ApiService.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001/grade/';
+
